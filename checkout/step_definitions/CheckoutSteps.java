16257bbaa176fcf5eca9eee3a476a4d9d26aa5d3
svivaramneni <sridharrao85@gmail.com>
2017-07-27 18:37:12 -0400

    Cucumber prototype commit

---
+++ checkout/step_definitions/CheckoutSteps.java
@@ -0,0 +1,27 @@
+package step_definitions;
+
+import cucumber.api.java.en.*;
+import cucumber.api.PendingException;
+import implementation.Checkout;
+import static org.junit.Assert.*;
+
+public class CheckoutSteps {
+    Checkout checkout;
+    int bananaPrice = 0;
+
+    @Given("^the price of a \"(.*?)\" is (\\d+)c$")
+    public void thePriceOfAIsC(String name, int price) throws Throwable {
+        bananaPrice = price;
+    }
+
+    @When("^I checkout (\\d+) \"(.*?)\"$")
+    public void iCheckout(int itemCount, String itemName) throws Throwable {
+        checkout = new Checkout();
+        checkout.add(itemCount, bananaPrice);
+    }
+
+    @Then("^the total price should be (\\d+)c$")
+    public void theTotalPriceShouldBeC(int total) throws Throwable {
+        assertEquals(total, checkout.total());
+    }
+}
